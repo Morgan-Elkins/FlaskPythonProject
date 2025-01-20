90b01a9358612ea49e43820db27b1692442b9c5e
Morgan-Elkins <me839@york.ac.uk>
2025-01-20 14:54:09 +0000

    Added get actors by film id

--- api/routes/film_actors.py
+++ api/routes/film_actors.py
@@ -17,8 +17,9 @@ def read_all_film_actors():
     print("HELLO")
     return actors_schema.dump(filaActors)
 
-# GET requests to a specific document
-@film_actors_router.get('/<actor_id>')
+# GET requests to return all films be an actor
+# localhost:5000/api/film_actors/get_films/500
+@film_actors_router.get('/get_films/<actor_id>')
 def read_films_by_actor(actor_id):
     actors = Actor.query.get(actor_id)
 
@@ -26,3 +27,12 @@ def read_films_by_actor(actor_id):
             return f"The actor id: {actor_id} is invalid", 404
 
     return films_schema.dump(actors.film)
+
+@film_actors_router.get('/get_actors/<film_id>')
+def read_actors_by_film(film_id):
+    films = Film.query.get(film_id)
+
+    if films == None:
+            return f"The actor id: {film_id} is invalid", 404
+
+    return actors_schema.dump(films.actor)
